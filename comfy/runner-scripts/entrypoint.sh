63fc6b2f24930f6b6eaa58f312ceb6a363938365
tr4shp4ndu <46363873+Tr4shP4ndu@users.noreply.github.com>
2024-09-01 16:38:08 +0100

    commit

---
+++ comfy/runner-scripts/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Install ComfyUI
+cd /root
+if [ ! -f "/root/.download-complete" ] ; then
+    chmod +x /runner-scripts/download.sh
+    bash /runner-scripts/download.sh
+fi ;
+
+
+echo "########################################"
+echo "[INFO] Starting ComfyUI..."
+echo "########################################"
+
+# Let .pyc files be stored in one place
+export PYTHONPYCACHEPREFIX="/root/.cache/pycache"
+# Let PIP install packages to /root/.local
+export PIP_USER=true
+# Add above to PATH
+export PATH="${PATH}:/root/.local/bin"
+# Suppress [WARNING: Running pip as the 'root' user]
+export PIP_ROOT_USER_ACTION=ignore
+
+cd /root
+
+python3 ./ComfyUI/main.py --listen --port 8188 ${CLI_ARGS}
